7547b6e8ba3b2a9197b2cc8084d838a421ebc8ed
nandakhoerunissa <109189382+nandakhoerunissa@users.noreply.github.com>
2022-10-19 11:37:27 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,14 +3,34 @@
 <head>
     <title>Pendaftaran Pasien Rumah Sakit</title>
 </head>
+<style>
+<body>{
+background-image:url('buah-segar.jpg');
+color:white;
+font-family:Arial:
+}
+.action-btn {
+    display:inline-block;
+    text-decoration:none;
+    color:white:
+    font-wight:700:
+    background:linear-gradient(90deg,#a701c8,#55e7fc);
+    padding:0.5em 2em;
+    border-radius:60px;
+    margin:1em 0;
+    transition:0,3s;
+}
+</style>
+</head>
 <body>
-    <h2>Rumah Sakit</h2>
+    <center>
+    <h1>Rumah Sakit</h1>
     <h2>Sehat Waras Sayung</h2>
+        <h4> <a href ="profil.php">Profil</a></h4>
+        <h4> <a href ="pasien.php">Lihat Pasien</a></h4>
+        <h4> <a href ="tambah.php">Tambah Pasien</a></h4>
+        <h4> <a href ="fasilitas.php">Fasilitas</a></h4>
+        <table border="1">
+</center>
 </body>
-    <ul>
-        <li> <a href ="profil.php">Profil</a></li>
-        <li> <a href ="pasien.php">Lihat Pasien</a></li>
-        <li> <a href ="tambah.php">Tambah Pasien</a></li>
-        <li> <a href ="fasilitas.php">Fasilitas</a></li>
-</ul>
 </html>
